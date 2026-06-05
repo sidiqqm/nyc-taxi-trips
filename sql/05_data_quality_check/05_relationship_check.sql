67cda9b60d19f7d5197facd185dd388692e4da8f
sidiqqm <moehammad.siddiq@gmail.com>
2026-06-05 23:53:27 +0700

    minor update

---
+++ sql/05_data_quality_check/05_relationship_check.sql
@@ -0,0 +1,25 @@
+WITH cleaned_trips AS (
+    SELECT
+        trip_id,
+        pickup_location_id,
+        dropoff_location_id
+    FROM `your_project_id.nyc_taxi_staging.stg_yellow_taxi_trips_cleaned`
+),
+
+zone_lookup AS (
+    SELECT
+        LocationID AS location_id
+    FROM `your_project_id.nyc_taxi_raw.taxi_zone_lookup`
+)
+
+SELECT
+    COUNT(*) AS total_rows,
+
+    COUNTIF(pu.location_id IS NULL) AS invalid_pickup_location_id,
+    COUNTIF(doz.location_id IS NULL) AS invalid_dropoff_location_id
+
+    FROM cleaned_trips AS trips
+        LEFT JOIN zone_lookup AS pu
+    ON trips.pickup_location_id = pu.location_id
+        LEFT JOIN zone_lookup AS doz
+    ON trips.dropoff_location_id = doz.location_id;

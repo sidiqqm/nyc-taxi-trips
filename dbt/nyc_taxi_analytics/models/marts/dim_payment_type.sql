410b56277a74af3552a7a17d12f8c9a42119e2c9
sidiqqm <moehammad.siddiq@gmail.com>
2026-06-19 00:00:09 +0700

    minor update

--- dbt/nyc_taxi_analytics/models/marts/dim_payment_type.sql
+++ dbt/nyc_taxi_analytics/models/marts/dim_payment_type.sql
@@ -11,5 +11,5 @@ FROM UNNEST([
     STRUCT(2 AS payment_type_id, 'Cash' AS payment_type_name, 'Cash payment' AS payment_category, TRUE AS is_standard_payment_type),
     STRUCT(3 AS payment_type_id, 'No charge' AS payment_type_name, 'Non-revenue' AS payment_category, TRUE AS is_standard_payment_type),
     STRUCT(4 AS payment_type_id, 'Dispute' AS payment_type_name, 'Exception' AS payment_category, TRUE AS is_standard_payment_type),
-    STRUCT(5 AS payment_type_id, 'Unknown' AS payment_type_name, 'Unknown' AS payment_category, TRUE AS is_standard_payment_type),
+    STRUCT(5 AS payment_type_id, 'Unknown' AS payment_type_name, 'Unknown' AS payment_category, TRUE AS is_standard_payment_type)
 ])

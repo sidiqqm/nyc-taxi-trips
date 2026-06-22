4c2ec5fb6c33bb97da4cc42100b7191fc699f5cd
sidiqqm <moehammad.siddiq@gmail.com>
2026-06-22 23:17:17 +0700

    upd/demand heatmap

---
+++ dbt/nyc_taxi_analytics/models/marts/reporting/rpt_demand_heatmap.sql
@@ -0,0 +1,72 @@
+{{config(materialized='view')}}
+
+WITH calendar AS (
+    SELECT
+        full_date,
+        day_of_week,
+        day_name,
+        is_weekend
+    FROM {{ref('dim_date')}}
+    WHERE full_date BETWEEN '2023-01-01' AND '2023-12-31'
+),
+
+hours AS (
+    SELECT
+        hour_value AS pickup_hour
+    FROM UNNEST(GENERATE_ARRAY(0, 23)) AS hour_value
+)
+
+daily_hourly_grid AS (
+    SELECT
+        c.full_date,
+        c.day_of_week,
+        c.day_name,
+        c.is_weekend,
+        h.pickup_hour,
+        COUNT(f.trip_id) AS total_trips,
+
+    FROM calendar c
+    CROSS JOIN hours h
+
+    LEFT JOIN {{ref('fact_yellow_taxi_trips')}}
+        ON c.full_date = f.pickup_date
+        AND h.pickup_hour = f.pickup_hour
+    
+    GROUP BY
+        c.full_date,
+        c.day_of_week,
+        c.day_name,
+        c.is_weekend,
+        h.pickup_hour
+)
+
+heatmap_metrics AS (
+    SELECT
+        day_of_week,
+        day_name,
+        is_weekend,
+        pickup_hour,
+
+        SUM(total_trips) AS total_trips,
+        ROUND(AVG(total_trips), 2) AS avg_trip_per_day
+    FROM daily_hourly_grid
+    GROUP BY 
+        day_of_week,
+        day_name,
+        is_weekend,
+        pickup_hour
+)
+
+SELECT
+    day_of_week,
+    day_name,
+    is_weekend,
+    pickup_hour,
+    total_trips,
+    avg_trip_per_day,
+    RANK() OVER(
+        ORDER BY avg_trip_per_day DESC
+    ) AS demand_rank
+
+FROM heatmap_metrics
+ORDER BY day_of_week, pickup_hour
---
+++ dbt/nyc_taxi_analytics/models/marts/reporting/rpt_hourly_demand.sql
@@ -0,0 +1,109 @@
+"""
+
+Jam berapa demand tertinggi?
+Apakah jam dengan demand tertinggi sama dengan revenue tertinggi?
+Apakah late night memiliki trip lebih sedikit tetapi revenue per trip lebih tinggi?
+
+"""
+
+{{ config(materialized='view') }}
+
+WITH hours AS (
+    SELECT
+        hour_value AS pickup_hour,
+
+        CASE
+            WHEN hour_value BETWEEN 0 AND 5 THEN 'Late night'
+            WHEN hour_value BETWEEN 6 AND 10 THEN 'Morning'
+            WHEN hour_value BETWEEN 11 AND 15 THEN 'Midday'
+            WHEN hour_value BETWEEN 16 AND 20 THEN 'Evening'
+            WHEN hour_value BETWEEN 21 AND 23 THEN 'Night'
+            ELSE 'Unknown'
+        END AS pickup_time_period
+
+    FROM UNNEST(GENERATE_ARRAY(0, 23)) AS hour_value
+),
+
+hourly_metrics AS (
+    SELECT
+        h.pickup_hour,
+        h.pickup_time_period,
+
+        COUNT(f.trip_id) AS total_trips,
+        COUNT(DISTINCT f.pickup_date) AS active_days,
+
+        ROUND(
+            SAFE_DIVIDE(
+                COUNT(f.trip_id),
+                COUNT(DISTINCT f.pickup_date)
+            ),
+            2
+        ) AS avg_trips_per_active_day,
+
+        ROUND(COALESCE(SUM(f.total_amount), 0), 2) AS total_revenue,
+        ROUND(AVG(f.total_amount), 2) AS avg_revenue_per_trip,
+
+        ROUND(AVG(f.trip_distance), 2) AS avg_trip_distance,
+        ROUND(AVG(f.trip_duration_minutes), 2) AS avg_trip_duration_minutes
+
+    FROM hours h
+
+    LEFT JOIN {{ ref('fact_yellow_taxi_trips') }} AS fact f
+        ON h.pickup_hour = f.pickup_hour
+
+    GROUP BY
+        h.pickup_hour,
+        h.pickup_time_period
+),
+
+ranked_hours AS (
+    SELECT
+        pickup_hour,
+        pickup_time_period,
+
+        total_trips,
+        active_days,
+        avg_trips_per_active_day,
+
+        total_revenue,
+        avg_revenue_per_trip,
+
+        avg_trip_distance,
+        avg_trip_duration_minutes,
+
+        RANK() OVER (
+            ORDER BY total_trips DESC
+        ) AS demand_rank,
+
+        RANK() OVER (
+            ORDER BY total_revenue DESC
+        ) AS revenue_rank,
+
+        ROUND(
+            SAFE_DIVIDE(total_trips, SUM(total_trips) OVER ()) * 100,
+            2
+        ) AS trip_share_percentage
+
+    FROM hourly_metrics
+)
+
+SELECT
+    pickup_hour,
+    pickup_time_period,
+
+    total_trips,
+    active_days,
+    avg_trips_per_active_day,
+
+    total_revenue,
+    avg_revenue_per_trip,
+
+    avg_trip_distance,
+    avg_trip_duration_minutes,
+
+    demand_rank,
+    revenue_rank,
+    trip_share_percentage
+
+FROM ranked_hours
+ORDER BY pickup_hour
